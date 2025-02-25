1e1c8bde86713276ea39401be087e8995483265c
jot-s-bindra <jotsbindra03@gmail.com>
2025-02-26 03:19:40 +0530

    to upoad to ec2

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,8 +1,46 @@
+// import axios from 'axios';
+
+// export const login = async (uid, pwd, batch) => {
+//     try {
+//         const response = await axios.post('http://localhost:5112/api/student/details', {
+//             uid,
+//             pwd,
+//             batch
+//         }, {
+//             withCredentials: true 
+//         });
+//         return response.data.status === 'success';
+//     } catch (error) {
+//         console.error("Login failed:", error);
+//         return false;
+//     }
+// };
+
+// export const isAuthenticated = async () => {
+//     try {
+//         const response = await axios.get('http://localhost:4000/api/auth/verify', {
+//             withCredentials: true 
+//         });
+//         return response.status === 200;
+//     } catch (error) {
+//         return false;
+//     }
+// };
+
+
+
+// export const logout = async () => {
+//     try {
+//         await axios.post('http://localhost:4000/api/auth/logout', {}, { withCredentials: true });
+//     } catch (error) {
+//         console.error('Logout failed:', error);
+//     }
+// };
 import axios from 'axios';
 
 export const login = async (uid, pwd, batch) => {
     try {
-        const response = await axios.post('http://localhost:5112/api/student/details', {
+        const response = await axios.post('http://54.252.9.15:5112/api/student/details', {
             uid,
             pwd,
             batch
@@ -18,7 +56,7 @@ export const login = async (uid, pwd, batch) => {
 
 export const isAuthenticated = async () => {
     try {
-        const response = await axios.get('http://localhost:4000/api/auth/verify', {
+        const response = await axios.get('http://54.252.9.15:4000/api/auth/verify', {
             withCredentials: true 
         });
         return response.status === 200;
